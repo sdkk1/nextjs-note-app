132baf6b73accfa7d56a2d1a7ff4a89a04d654ab
Keita <fast.break.baske03@gmail.com>
2022-09-22 00:26:01 +0900

    add: APPページを作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,53 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+import { supabase } from '@/utils/supabase'
+
+import '../styles/globals.css'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { push, pathname } = useRouter()
+
+  supabase.auth.onAuthStateChange((event) => {
+    if (event === 'SIGNED_IN' && pathname === '/') {
+      push('/notes')
+    }
+    if (event === 'SIGNED_OUT' && pathname !== '/') {
+      push('/')
+    }
+  })
+
+  const validateSession = async () => {
+    const user = supabase.auth.user()
+    if (user && pathname === '/') {
+      push('/notes')
+    } else if (!user && pathname !== '/') {
+      await push('/')
+    }
+  }
+
+  useEffect(() => {
+    validateSession()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
